2ad0efc816ad6ce7367bf2db090b719f70d21496
Galen White <galenhwhite419@gmail.com>
2023-03-31 14:41:32 -0400

    Pseudocode typed up

---
+++ src/components/BikeOrWalk.js
@@ -0,0 +1,18 @@
+const BikeOrWalk = () => {
+    // Discuss with client:
+        // We're not sure what the travel time will be until we suggest biking or walking. 
+        // Are we prioritizing travel length or podcast length? 
+        // We suggest instead displaying both walking time and biking time
+            // Then user can choose walking or biking, and then have a podcast recommended to them based on travel time
+
+    // Conditional that factors in the trip distance
+        // If over a certain distance, suggest bike
+            // Render message that recommends not using headphone while biking
+        // Otherwise, suggest walk
+
+    return (
+        <p>Placeholder text because I hate React for giving me errors.</p>
+    )
+}
+
+export default BikeOrWalk;
---
+++ src/components/Form.js
@@ -0,0 +1,35 @@
+// Import useState hook
+const Form = () => {
+    // useState for location - string
+    // useState for destination - string
+    // useState for podcast search results - array
+    // useState for error message - boolean
+
+    // Onchange listener for starting location
+    // Onchange listener for destination
+    // Onchange listener for podcast serach
+
+    // Function that handles when location form is submitted
+         // Location axios call that triggers when map form is submitted
+
+    // Function that handles when podcast form is submitted
+        // Podcast axios call that triggers when podcast form is submitted
+
+    return (
+        <form action=""></form>
+        // Location form
+            // Text input and label for starting location
+            // Text input and label for destination
+            // Submit button
+        // Podcast form
+            // Text input with label that allows user to search for a podcast
+            // Submit button
+
+            // PodcastInfo component
+
+            // MapDisplay component
+            // BikeOrWalk component
+    )
+}
+
+export default Form;
