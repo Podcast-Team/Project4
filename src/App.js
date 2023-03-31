16d4ff101e806bdef5c7a2f616c15da1d6913cfc
Galen White <galenhwhite419@gmail.com>
2023-03-31 15:25:15 -0400

    Created successful data calls for APIs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 // Import components
+import Form from './components/Form.js';
 
 function App() {
   return (
     <div className="App">
         {/* main container */}
-          {/* Form component */}
+          <Form />
         {/* /main */}
         {/* footer crediting the homies */}
     </div>
