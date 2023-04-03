f79d8ea781071ff8964a1518a96e0d8759929757
Galen White <galenhwhite419@gmail.com>
2023-04-03 16:09:47 -0400

    Trying to resolve sass issues

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import './styles/sass/style.css';
-import Form from './components/Form.js';
+import "./styles/sass/style.css";
+import Form from "./components/Form.js";
 
 function App() {
   return (
     <div className="App">
       <main>
+        <div className="wrapper">
           <Form />
+        </div>
       </main>
-<footer>
-  <p>Designed by: 
-    <a target="_blank" href="https://adrienpolselli.com/"> Adrien Polselli,</a> 
-    <a target="_blank" href="https://derekngan.com/"> Derek Ngan,</a> 
-    <a target="_blank" href="https://galenwhitecodes.tech"> Galen White,</a> and
-    <a target="_blank" href="https://jephchen.com/"> Jeff Chen </a> 
-  2023 at Juno College</p>
-  </footer>
+      <footer>
+        <div className="wrapper">
+          <p>
+            Designed by:
+            <a target="_blank" href="https://adrienpolselli.com/">
+              {" "}
+              Adrien Polselli,
+            </a>
+            <a target="_blank" href="https://derekngan.com/">
+              {" "}
+              Derek Ngan,
+            </a>
+            <a target="_blank" href="https://galenwhitecodes.tech">
+              {" "}
+              Galen White,
+            </a>{" "}
+            and
+            <a target="_blank" href="https://jephchen.com/">
+              {" "}
+              Jeff Chen{" "}
+            </a>
+            2023 at Juno College
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
