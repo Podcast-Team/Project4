1d7cb653b8292e0821145e4e4ba02c07f51a8564
Galen White <galenhwhite419@gmail.com>
2023-04-10 17:13:35 -0400

    Cleaned up code a little bit, preparing to add comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,3 @@
-// Import useState hook
 import axios from "axios";
 import { useState } from "react";
 import BikeOrWalk from "./BikeOrWalk.js";
@@ -6,37 +5,27 @@ import PodcastInfo from "./PodcastInfo.js";
 import TravelFrom from "./TravelForm.js";
 
 const Form = () => {
-  // useEffect(() => {}, []);
   const [location, setLocation] = useState("");
   const [destination, setDestination] = useState("");
+
   const [podcastSearch, setPodcastSearch] = useState("");
-  const [error, setError] = useState(false);
+  const [podcastList, setPodcastList] = useState([]);
+
   const [walkTime, setWalkTime] = useState("");
   const [bikeTime, setBikeTime] = useState("");
-  const [podcastList, setPodcastList] = useState([]);
-  const [message, setMessage] = useState("");
   const [mapRoute, setMapRoute] = useState("");
+  
+  const [message, setMessage] = useState("");
 
-  // Onchange listener for starting location
-  // Onchange listener for destination
-  // Onchange listener for podcast search
-
-  // Function that handles when location form is submitted
-  // Location axios call that triggers when map form is submitted
-
-  // Function that handles when podcast form is submitted
-  // Podcast axios call that triggers when podcast form is submitted
-  let newLocation
+  let newLocation;
   const handleLocationChange = (e, location) => {
-    newLocation = location.split(",").slice(0,2).join(',');
-    console.log(newLocation)
-    setLocation(newLocation)
+    newLocation = location.split(",").slice(0, 2).join(",");
+    setLocation(newLocation);
   };
-  let newDestination
+  let newDestination;
   const handleDestinationChange = (e, destination) => {
-   newDestination = destination.split(",").slice(0,2).join(',');
-   console.log(newDestination)
-   setDestination(newDestination)
+    newDestination = destination.split(",").slice(0, 2).join(",");
+    setDestination(newDestination);
   };
 
   const handlePodcastSearchChange = (e) => {
@@ -54,10 +43,8 @@ const Form = () => {
 
     if (newLocation.trim() === "" || newDestination.trim() === "") {
       setMessage("Please enter a valid search");
-      
     } else {
       setMessage("Please wait, calculating route");
-      
 
       axios({
         url: "https://www.mapquestapi.com/directions/v2/route",
@@ -68,18 +55,19 @@ const Form = () => {
           from: newLocation,
           to: newDestination,
           routeType: "pedestrian",
-        },   
-      }).then((resp) => {
-        console.log(resp)
-        if (resp.data.info.statuscode === 0) {
-          setWalkTime(resp.data.route.formattedTime);
-        } else {
-          setWalkTime ("0")
-          setMessage ("Sorry, no results were found")
-        }
-      }).catch(() => {
-        setMessage("Sorry, something went wrong. Try again shortly!")
+        },
       })
+        .then((resp) => {
+          if (resp.data.info.statuscode === 0) {
+            setWalkTime(resp.data.route.formattedTime);
+          } else {
+            setWalkTime("0");
+            setMessage("Sorry, no results were found");
+          }
+        })
+        .catch(() => {
+          setMessage("Sorry, something went wrong. Try again shortly!");
+        });
 
       axios({
         url: "https://www.mapquestapi.com/directions/v2/route",
@@ -91,31 +79,32 @@ const Form = () => {
           to: newDestination,
           routeType: "bicycle",
         },
-      }).then((resp) => {
-        if (resp.data.info.statuscode === 0) {
-          setBikeTime(resp.data.route.formattedTime);
-          setMessage("");
-        } else {
-          setBikeTime ("0")
-          setMessage ("Sorry, no results were found")
-        }
-      }).catch(() => {
-        setMessage("Sorry, something went wrong. Try again shortly!")
       })
+        .then((resp) => {
+          if (resp.data.info.statuscode === 0) {
+            setBikeTime(resp.data.route.formattedTime);
+            setMessage("");
+          } else {
+            setBikeTime("0");
+            setMessage("Sorry, no results were found");
+          }
+        })
+        .catch(() => {
+          setMessage("Sorry, something went wrong. Try again shortly!");
+        });
     }
-      axios({
-        url: "https://www.mapquestapi.com/staticmap/v5/map",
-        method: "GET",
-        dataResponse: "json",
-        params: {
-          key: "GajCx4GDQ4BbxuYSyMwSYdn9B65f9Vnx",
-          start: newLocation,
-          end: newDestination,
-        },
-      }).then((resp)=>{
-        setMapRoute(resp.request.responseURL)
-        console.log(resp.request.responseURL)
-      })
+    axios({
+      url: "https://www.mapquestapi.com/staticmap/v5/map",
+      method: "GET",
+      dataResponse: "json",
+      params: {
+        key: "GajCx4GDQ4BbxuYSyMwSYdn9B65f9Vnx",
+        start: newLocation,
+        end: newDestination,
+      },
+    }).then((resp) => {
+      setMapRoute(resp.request.responseURL);
+    });
   };
   // const timeWalk = walkTime;
   const [walkHours, walkMinutes, walkSeconds] = walkTime.split(":").map(Number);
@@ -128,7 +117,6 @@ const Form = () => {
   const totalBikeMinutes = Math.round(
     bikeHours * 60 + bikeMinutes + bikeSeconds / 60
   );
-
 
   const onSubmitPodSearch = (e) => {
     e.preventDefault();
@@ -172,50 +160,37 @@ const Form = () => {
               "Sorry, we couldn't find a podcast like that, try a different search!"
             );
           } else {
-            console.log(response.data.results);
             setPodcastList(
               response.data.results.map((list) => {
-                setMessage("Here are some podcasts you can listen to on your trip!");
+                setMessage(
+                  "Here are some podcasts you can listen to on your trip!"
+                );
                 return list;
               })
             );
           }
         })
         .catch((error) => {
-          setMessage("Sorry, we're having trouble finding any podcasts. Try again shortly!");
-          // setUserChoice("");
+          setMessage(
+            "Sorry, we're having trouble finding any podcasts. Try again shortly!"
+          );
         });
     }
   };
 
   return (
     <>
-    <TravelFrom submit={onSubmitLocation} setLocation={handleLocationChange} setDestination={handleDestinationChange}    />
-      {/* <form action="submit" onSubmit={onSubmitLocation} className="travelForm" >
-        <label htmlFor="location" className="sr-only">
-          Where are you starting from?
-        </label>
-        <input
-          onChange={handleLocationChange}
-          value={location}
-          type="text"
-          id="location"
-          placeholder="Where are you starting from?"
-        />
-        <label htmlFor="destination" className="sr-only">
-          Where are you headed?
-        </label>
-        <input
-          onChange={handleDestinationChange}
-          value={destination}
-          type="text"
-          id="destination"
-          placeholder="Where are you headed?"
-        />
-        <button type="submit">Submit</button>
-      </form> */}
+      <TravelFrom
+        submit={onSubmitLocation}
+        setLocation={handleLocationChange}
+        setDestination={handleDestinationChange}
+      />
 
-      <form action="submit" onSubmit={onSubmitPodSearch} className="podcastForm">
+      <form
+        action="submit"
+        onSubmit={onSubmitPodSearch}
+        className="podcastForm"
+      >
         <label htmlFor="podSearch">
           What podcast do you want to listen to?
         </label>
@@ -246,15 +221,6 @@ const Form = () => {
         userChoice={userChoice}
       />
     </>
-
-    // Podcast form
-    // Text input with label that allows user to search for a podcast
-    // Submit button
-
-    // PodcastInfo component
-
-    // MapDisplay component
-    // BikeOrWalk component
   );
 };
 
